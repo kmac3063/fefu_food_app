b2150344b8ced0f908c4b236a24bc1c127fa29ef
ephemeralsad <sarancev.dev@gmail.com>
2020-10-04 02:45:13 +1000

    Commit

--- app/src/main/java/com/example/fefu_food_app/ProfileActivity.java
+++ app/src/main/java/com/example/fefu_food_app/ProfileActivity.java
@@ -12,28 +12,5 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        findViewById(R.id.image_view28).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                LayoutInflater layoutInflater = (LayoutInflater) MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                View customView = layoutInflater.inflate(R.layout.popup,null);
-
-                closePopupBtn = (Button) customView.findViewById(R.id.closePopupBtn);
-
-                //instantiate popup window
-                popupWindow = new PopupWindow(customView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-
-                //display the popup window
-                popupWindow.showAtLocation(linearLayout1, Gravity.CENTER, 0, 0);
-
-                //close the popup window on button click
-                closePopupBtn.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        popupWindow.dismiss();
-                    }
-                });
-            }
-        });
     }
 }
