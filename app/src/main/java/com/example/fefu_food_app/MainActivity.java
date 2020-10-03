4d538cfffa9a5c56cb982424fe8c1f9294ed25bc
ephemeralsad <sarancev.dev@gmail.com>
2020-10-04 05:40:51 +1000

    Commit

--- app/src/main/java/com/example/fefu_food_app/MainActivity.java
+++ app/src/main/java/com/example/fefu_food_app/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     EditText mEditText1;
