2b6ab09fc25e8ceed88a3eb76b5e7f32791f5967
lancov <lantcov.iiu@students.dvfu.ru>
2020-10-04 04:28:19 +1000

    New Page

--- app/src/main/java/com/example/fefu_food_app/BasketActivity.java
+++ app/src/main/java/com/example/fefu_food_app/BasketActivity.java
@@ -1,82 +1,44 @@
 package com.example.fefu_food_app;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.app.Dialog;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.fefu_food_app.model.Money;
-import com.example.fefu_food_app.model.Order;
 import com.example.fefu_food_app.model.Product;
-import com.example.fefu_food_app.model.ProductCategory;
-import com.example.fefu_food_app.model.UserData;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class ProductsActivity extends AppCompatActivity {
-
+public class BasketActivity extends AppCompatActivity {
     RecyclerView mRecyclerView;
-
-    ImageView imV;
-    TextView txV;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_products);
+        setContentView(R.layout.activity_basket);
 
-        ProductCategory cat = ProductCategory.values()[getIntent().getIntExtra("FIRST", -1) - 1];
-        List<Product> list = UserData.getUserData().getProducts(cat);
-
-        mRecyclerView = findViewById(R.id.products_recycler);
+        mRecyclerView = findViewById(R.id.noproducts_recycler);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        ProdAdapter adapter = new ProdAdapter(list);
-
-        mRecyclerView.setAdapter(adapter);
-
-        imV = findViewById(R.id.image_view01);
-        imV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
-
-        txV = findViewById(R.id.back_text1);
-        txV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+        //UserData.getUserData().currentOrder.getList()
+//        ProdAdapter adapter = new ProdAdapter();
+//        mRecyclerView.setAdapter(adapter);
     }
-
-
 
     private class ProdAdapter extends RecyclerView.Adapter<ProdAdapter.ProdViewHolder> {
 
         List<Product> mProducts;
         @NonNull
         @Override
-        public ProdViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(ProductsActivity.this)
+        public ProdAdapter.ProdViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(BasketActivity.this)
                     .inflate(R.layout.layout_recycle_item, parent, false);
-            return new ProdViewHolder(view);
+            return new ProdAdapter.ProdViewHolder(view);
         }
 
         ProdAdapter(List<Product> prods) {
@@ -84,7 +46,7 @@ public class ProductsActivity extends AppCompatActivity {
         }
 
         @Override
-        public void onBindViewHolder(@NonNull ProdViewHolder holder, int position) {
+        public void onBindViewHolder(@NonNull ProdAdapter.ProdViewHolder holder, int position) {
             Product p = mProducts.get(position);
             holder.bind(p);
         }
@@ -106,6 +68,7 @@ public class ProductsActivity extends AppCompatActivity {
 
             public void bind(Product p) {
                 mTextView.setText(p.getName());
+                mTextView.setText("Удалить");
             }
         }
     }
