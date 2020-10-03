41b96e2f7554b4284dbb77e9024496d918c57fb8
ephemeralsad <sarancev.dev@gmail.com>
2020-10-04 06:02:35 +1000

    Commit

--- app/src/main/java/com/example/fefu_food_app/BoardActivity.java
+++ app/src/main/java/com/example/fefu_food_app/BoardActivity.java
@@ -14,19 +14,5 @@ public class BoardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
-
-        ArrayList<Pair<String, String>> spisok_for_IGOR = new ArrayList<>(Arrays.asList(
-                new Pair<String, String>("Иванов Александр", "300 руб"),
-                new Pair<String, String>("Александров Иван", "540 руб"),
-                new Pair<String, String>("Волобуев ВотВашМеч", "202 руб"),
-                new Pair<String, String>("Сонный Игорь", "511 руб"),
-                new Pair<String, String>("Андроидный Студий", "200 руб"),
-                new Pair<String, String>("Петров Петр", "1300 руб"),
-                new Pair<String, String>("Сидоров Дмитрий", "190 руб"),
-                new Pair<String, String>("Бульба Наталья", "280 руб"),
-                new Pair<String, String>("Восточная Марья", "700 руб"),
-                new Pair<String, String>("Цифровой Прорыв", "3000 руб"),
-                new Pair<String, String>("Гейтс Билл", "560 руб")
-        ));
     }
 }
