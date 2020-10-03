8339a0e1d4c6270f42e7bd25ab9e419ad507f00a
ephemeralsad <sarancev.dev@gmail.com>
2020-10-04 02:49:33 +1000

    Commit

--- app/src/main/java/com/example/fefu_food_app/ProfileActivity.java
+++ app/src/main/java/com/example/fefu_food_app/ProfileActivity.java
@@ -2,15 +2,29 @@ package com.example.fefu_food_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    ImageView mImageViewClient;
+    ImageView mImageViewCostumer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        mImageViewClient = findViewById(R.id.image_view29);
+        mImageViewClient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ProfileActivity.this, CategoriesActivity.class);
+                startActivity(i);
+            }
+        });
+        mImageViewCostumer = findViewById(R.id.image_view28);
     }
 }
