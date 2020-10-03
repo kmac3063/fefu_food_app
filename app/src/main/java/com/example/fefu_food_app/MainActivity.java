bbb7cc165c28b884953f8f7666867cf7faedacba
lancov <lantcov.iiu@students.dvfu.ru>
2020-10-03 18:56:05 +1000

    Complete start screen

--- app/src/main/java/com/example/fefu_food_app/MainActivity.java
+++ app/src/main/java/com/example/fefu_food_app/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.fefu_food_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText mEditText1;
+    TextView mSendSMSTextView;
+    TextView mGoNextTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditText1 = findViewById(R.id.edit_text1);
+        mSendSMSTextView = findViewById(R.id.text3);
+        mGoNextTextView = findViewById(R.id.text4);
     }
 }
