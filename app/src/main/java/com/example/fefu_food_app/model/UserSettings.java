2d258f2f09f64e16ea6aad1eb1de3769bd74a5ad
ephemeralsad <sarancev.dev@gmail.com>
2020-10-04 00:44:05 +1000

    Added something

--- app/src/main/java/com/example/fefu_food_app/model/UserSettings.java
+++ app/src/main/java/com/example/fefu_food_app/model/UserSettings.java
@@ -7,12 +7,15 @@ import com.example.fefu_food_app.model.Order;
 import java.util.ArrayList;
 
 public class UserSettings {
+    public String phone;
     public Bitmap profileImage;
     public String name;
     public String surname;
+    public String card;
     public String cardExpiryDate;
     public String cvv;
     public String corpus;
     public String room;
+
     public ArrayList<Order> orders;
 }
