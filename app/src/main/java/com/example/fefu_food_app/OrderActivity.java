c266d5e8c2d40e80111a9275784fcd18d1a6053f
lancov <lantcov.iiu@students.dvfu.ru>
2020-10-04 14:21:08 +1000

    Board Activity

--- app/src/main/java/com/example/fefu_food_app/OrderActivity.java
+++ app/src/main/java/com/example/fefu_food_app/OrderActivity.java
@@ -2,12 +2,22 @@ package com.example.fefu_food_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.fefu_food_app.model.Order;
 
 public class OrderActivity extends AppCompatActivity {
 
     private TextView mTextView;
+    private TextView mTextView1;
+    ImageView mImageView1;
+    ImageView mImageView2;
+    ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +30,34 @@ public class OrderActivity extends AppCompatActivity {
         mTextView = findViewById(R.id.ooresult_text_view);
         mTextView.setText(money_from_previous_activity);
 
+        mTextView1 = findViewById(R.id.ooomoyaoborona);
+        mImageView1 = findViewById(R.id.plusooссamimage_view1);
+        mImageView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = mTextView1.getText().toString();
+                mTextView1.setText("" + (Integer.valueOf(s) + 10));
+            }
+        });
+        mImageView2 = findViewById(R.id.minusooссamimage_view1);
+        mImageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = mTextView1.getText().toString();
+                if (Integer.valueOf(s) - 10 < 0) return;
+                mTextView1.setText("" + (Integer.valueOf(s) - 10));
+            }
+        });
 
+        mImageView = findViewById(R.id.imageView100);
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(OrderActivity.this, "Отлично! Ожидайте подтверждения заказчика.", Toast.LENGTH_SHORT)
+                        .show();
+                Intent i = new Intent(OrderActivity.this, MenuActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
